0f78934879d1f1a759b6d5cc80e7cd060de44fba
winkman <cralack@hotmail.com>
2022-03-11 18:34:40 +0800

    corporate_flight_bookings

--- Array/1109_corporate_flight_bookings/corporate_flight_bookings_test.go
+++ Array/1109_corporate_flight_bookings/corporate_flight_bookings_test.go
@@ -3,7 +3,20 @@ package corporateflightbookings
 import "testing"
 
 func corpFlightBookings(bookings [][]int, n int) []int {
+	dif := make([]int, n)
+	for _, update := range bookings {
+		i, j := update[0], update[1]
+		val := update[2]
+		dif[i-1] += val
+		if j < n {
+			dif[j] -= val
+		}
+	}
 	nums := make([]int, n)
+	nums[0] = dif[0]
+	for i := 1; i < n; i++ {
+		nums[i] = nums[i-1] + dif[i]
+	}
 	return nums
 }
 func Test_corporate_flight_bookings(t *testing.T) {
