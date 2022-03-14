1091bbb88da3ff787a85a6521319cdff80cad3d9
winkman <cralack@hotmail.com>
2022-03-14 18:33:06 +0800

    koko_eating_banana

---
+++ Array/875_koko_eating_bananas/koko_eating_bananas_test.go
@@ -0,0 +1,42 @@
+package kokoeatingbananas
+
+import (
+	"testing"
+)
+
+func minEatingSpeed(piles []int, h int) int {
+	lo, hi := 1, 0
+	for i := range piles {
+		if hi < piles[i] {
+			hi = piles[i]
+		}
+	}
+
+	for lo < hi { //O(logn)
+		mi := lo + (hi-lo)>>1
+		if possible(piles, mi, h) {
+			hi = mi
+		} else {
+			lo = mi + 1
+		}
+	}
+	return lo //O(n+nlogn)
+}
+func possible(piles []int, k, h int) bool {
+	time := 0 //O(n)
+	for _, pile := range piles {
+		time += (pile-1)/k + 1
+		if time > h {
+			return false
+		}
+	}
+	return time <= h
+}
+func Test_koko_eating_bananas(t *testing.T) {
+	piles, h := []int{3, 6, 7, 11}, 8
+	t.Log(minEatingSpeed(piles, h))
+	piles, h = []int{30, 11, 23, 4, 20}, 5
+	t.Log(minEatingSpeed(piles, h))
+	piles, h = []int{30, 11, 23, 4, 20}, 6
+	t.Log(minEatingSpeed(piles, h))
+}
