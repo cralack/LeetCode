386ab4957ca759e23012a5e6e6849defd8ad0ddc
winkman <cralack@hotmail.com>
2022-03-14 13:00:23 +0800

    rotate_image

---
+++ Array/48_rotate_image/rotate_image_test.go
@@ -0,0 +1,27 @@
+package rotateimage
+
+import "testing"
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	//沿对角线镜像
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	//reverse
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+		}
+	}
+}
+func Test_rotate_image(t *testing.T) {
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	rotate(matrix)
+	t.Log(matrix)
+	matrix = [][]int{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}}
+	rotate(matrix)
+	t.Log(matrix)
+}
