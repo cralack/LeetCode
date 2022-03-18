bd04d0628f96d01081a73e636608b10d3d87ef78
winkman <cralack@hotmail.com>
2022-03-18 11:29:40 +0800

    remove_element

--- ADT/27_remove_element/remove_element_test.go
+++ ADT/27_remove_element/remove_element_test.go
@@ -3,22 +3,18 @@ package removeelement
 import "testing"
 
 func removeElement(nums []int, val int) int {
-	lo, hi := 0, len(nums)-1
-	for lo < hi {
-		for nums[lo] != val {
-			lo++
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
 		}
-		for nums[hi] == val {
-			hi--
-		}
-		if lo < hi {
-			nums[lo], nums[hi] = nums[hi], nums[lo]
-		}
+		fast++
 	}
-	return lo
+	return slow
 }
 func Test_remove_element(t *testing.T) {
 	arr, val := []int{0, 1, 2, 2, 3, 0, 4, 2}, 2
-	t.Log(removeElement(arr, val))
+	arr = arr[:removeElement(arr, val)]
 	t.Log(arr)
 }
