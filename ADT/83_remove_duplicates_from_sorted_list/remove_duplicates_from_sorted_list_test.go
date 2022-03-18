6bf71fed0b67709985de2327d777a995cb26f811
winkman <cralack@hotmail.com>
2022-03-18 11:10:27 +0800

    remove_duplicates_from_sorted_list

---
+++ ADT/83_remove_duplicates_from_sorted_list/remove_duplicates_from_sorted_list_test.go
@@ -0,0 +1,29 @@
+package removeduplicatesfromsortedlist
+
+import (
+	. "Learning/Leetcode/List/MyList"
+	"testing"
+)
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast != nil {
+		if slow.Val != fast.Val {
+			slow.Next = fast
+			slow = slow.Next
+		}
+		fast = fast.Next
+	}
+	slow.Next = nil
+	return head
+}
+func Test_remove_duplicates_from_sorted_list(t *testing.T) {
+	arr := []int{1, 1, 2, 3, 3}
+	head := &ListNode{}
+	head.Init(arr)
+	deleteDuplicates(head)
+	head.Show()
+}
