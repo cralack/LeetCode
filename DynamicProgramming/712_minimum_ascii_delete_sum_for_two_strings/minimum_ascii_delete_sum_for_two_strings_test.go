f788d916cb390dcf6f3b970b3d981270370636c5
winkman <cralack@hotmail.com>
2022-03-23 18:54:34 +0800

    minimum_ascii_delete_sum_for_two_strings

--- DynamicProgramming/712_minimum_ascii_delete_sum_for_two_strings/minimum_ascii_delete_sum_for_two_strings_test.go
+++ DynamicProgramming/712_minimum_ascii_delete_sum_for_two_strings/minimum_ascii_delete_sum_for_two_strings_test.go
@@ -12,6 +12,7 @@ func minimumDeleteSum(s1 string, s2 string) int {
 		}
 		return res
 	}
+	//init
 	m, n := len(s1), len(s2)
 	dp := make([][]int, m+1)
 	for i := range dp {
@@ -31,8 +32,9 @@ func minimumDeleteSum(s1 string, s2 string) int {
 			if s1[i-1] == s2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
-				dp[i][j] = min(dp[i-1][j]+int(s1[i-1]),
-					dp[i][j-1]+int(s2[j-1]))
+				dp[i][j] = min(
+					dp[i-1][j]+int(s1[i-1]), // 删除 s1 中
+					dp[i][j-1]+int(s2[j-1])) // 删除 s2 中
 			}
 		}
 	}
