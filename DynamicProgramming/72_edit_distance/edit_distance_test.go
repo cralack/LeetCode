b66d4c1a1411e14c9c2c20cb78b5e8de7b108617
winkman <cralack@hotmail.com>
2022-03-23 15:41:54 +0800

    edit_distance

--- DynamicProgramming/72_edit_distance/edit_distance_test.go
+++ DynamicProgramming/72_edit_distance/edit_distance_test.go
@@ -30,25 +30,27 @@ func minDistance_DFS(word1 string, word2 string) int {
 	}
 	return dp(len(word1)-1, len(word2)-1)
 }
-func minDistance_DP(s1, s2 string) int {
+func minDistance_DP(word1, word2 string) int {
 	//init
-	m, n := len(s1), len(s2)
+	m, n := len(word1), len(word2)
+	if n*m == 0 {
+		return m + n
+	}
 	dp := make([][]int, m+1)
 	for i := range dp {
 		dp[i] = make([]int, n+1)
 	}
-	//s2=="" s1->base case
-	for i := 1; i <= n; i++ {
+	//if word2=="" word1->base case
+	for i := 1; i <= m; i++ {
 		dp[i][0] = i
 	}
-	//s1=="" s2->base case
+	//if word1=="" word2->base case
 	for j := 1; j <= n; j++ {
 		dp[0][j] = j
 	}
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
-			c1, c2 := string(s1[i-1]), string(s2[j-1])
-			if c1 == c2 {
+			if word1[i-1] == word2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
 				dp[i][j] = min(dp[i-1][j]+1, min(dp[i][j-1]+1, dp[i-1][j-1]+1))
@@ -59,33 +61,37 @@ func minDistance_DP(s1, s2 string) int {
 	return dp[m][n] //O(n^2)
 }
 func Test_edit_distance(t *testing.T) {
-	s1 := "horse"
-	s2 := "ros"
-	t.Log(minDistance_DFS(s1, s2))
+	word1 := "horse"
+	word2 := "ros"
+	t.Log(minDistance_DFS(word1, word2))
 
-	s1 = "intention"
-	s2 = "execution"
-	t.Log(minDistance_DP(s1, s2))
+	word1 = "intention"
+	word2 = "execution"
+	t.Log(minDistance_DP(word1, word2))
 
-	s1 = "dinitrophenylhydrazine"
-	s2 = "benzalphenylhydrazone"
-	t.Log(minDistance_DP(s1, s2))
+	word1 = "dinitrophenylhydrazine"
+	word2 = "benzalphenylhydrazone"
+	t.Log(minDistance_DP(word1, word2))
+
+	word1 = ""
+	word2 = "a"
+	t.Log(minDistance_DP(word1, word2))
 }
 
 func Benchmark_minDistance(b *testing.B) {
-	s1 := "punishment"
-	s2 := "production"
+	word1 := "punishment"
+	word2 := "production"
 	b.Run("DFS", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			minDistance_DFS(s1, s2)
+			minDistance_DFS(word1, word2)
 		}
 		b.StopTimer()
 	})
 	b.Run("DP", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			minDistance_DP(s1, s2)
+			minDistance_DP(word1, word2)
 		}
 		b.StopTimer()
 	})
