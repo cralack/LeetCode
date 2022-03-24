32fb8150a021aa71c2dacc05d613ba9b1f7821f0
winkman <cralack@hotmail.com>
2022-03-24 17:56:42 +0800

    longest_palindromic_subsequence

--- DynamicProgramming/516_longest_palindromic_subsequence/longest_palindromic_subsequence_test.go
+++ DynamicProgramming/516_longest_palindromic_subsequence/longest_palindromic_subsequence_test.go
@@ -5,21 +5,31 @@ import (
 )
 
 func longestPalindromeSubseq(s string) int {
-	dp := make([]int, len(s))
-	for i := range s {
-		rightOver := dp[i]
-		dp[i] = 1
-		for j := i - 1; j >= 0; j-- {
-			tmp := dp[j]
-			if s[i] == s[j] {
-				dp[j] = rightOver + 2
-			} else if dp[j+1] > dp[j] {
-				dp[j] = dp[j+1]
+	max := func(a ...int) int {
+		res := a[0]
+		for _, v := range a[1:] {
+			if res < v {
+				res = v
 			}
-			rightOver = tmp
+		}
+		return res
+	}
+	n := len(s)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
 		}
 	}
-	return dp[0]
+	return dp[0][n-1]
 }
 func Test_longest_palindromic_subsequence(t *testing.T) {
 	s := "bbbab"
