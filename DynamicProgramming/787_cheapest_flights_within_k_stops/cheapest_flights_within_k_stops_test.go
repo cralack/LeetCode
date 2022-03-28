0342fdfe91e493fda500c643e087bac6129a6197
winkman <cralack@hotmail.com>
2022-03-28 19:17:37 +0800

    add benchmark part

--- DynamicProgramming/787_cheapest_flights_within_k_stops/cheapest_flights_within_k_stops_test.go
+++ DynamicProgramming/787_cheapest_flights_within_k_stops/cheapest_flights_within_k_stops_test.go
@@ -1,6 +1,7 @@
 package cheapestflightswithinkstops
 
 import (
+	"container/heap"
 	"math"
 	"testing"
 )
@@ -88,9 +89,116 @@ func findCheapestPrice_ite(n int, flights [][]int, src int, dst int, k int) int 
 	}
 	return ans
 }
+
+type State struct {
+	id             int // 图节点的 id
+	distFromStart  int // 从 src 节点到当前节点的花费
+	nodeNumFromSrc int // 从 src 节点到当前节点经过的节点个数
+}
+type Heap []State
+
+func (h Heap) Less(i, j int) bool  { return h[i].distFromStart < h[j].distFromStart }
+func (h Heap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h Heap) Len() int            { return len(h) }
+func (h *Heap) Push(x interface{}) { *h = append(*h, x.(State)) }
+func (h *Heap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+func findCheapestPrice_Dijkstra(n int, flights [][]int, src int, dst int, k int) int {
+	//init graph
+	graph := make(map[int][][]int, n+1)
+	for _, edge := range flights {
+		from, to, wei := edge[0], edge[1], edge[2]
+		graph[from] = append(graph[from], []int{to, wei})
+	}
+	k++
+	return dijkstra(graph, src, dst, k, n)
+}
+
+// 输入一个起点 src，计算从 src 到其他节点的最短距离
+func dijkstra(graph map[int][][]int, src, dst, k, n int) int {
+	// 定义：从起点 src 到达节点 i 的最短路径权重为 distTo[i]
+	distTO := make([]int, n)
+	// 定义：从起点 src 到达节点 i 至少要经过 nodeNumTo[i] 个节点
+	nodeNumTO := make([]int, n)
+	for i := 0; i < n; i++ {
+		distTO[i] = math.MaxInt32
+		nodeNumTO[i] = math.MaxInt32
+	}
+	// base case
+	distTO[src] = 0
+	nodeNumTO[src] = 0
+	// 优先级队列，costFromSrc 较小的排在前面
+	pq := &Heap{{id: src, distFromStart: 0, nodeNumFromSrc: 0}}
+	for pq.Len() > 0 {
+		cur := heap.Pop(pq).(State)
+		curNodeid := cur.id
+		costFromSrc := cur.distFromStart
+		curNodeNumFromSrc := cur.nodeNumFromSrc
+		if curNodeid == dst {
+			// 找到最短路径
+			return costFromSrc
+		}
+		if curNodeNumFromSrc == k {
+			// 中转次数耗尽
+			continue
+		}
+		// 将 curNode 的相邻节点装入队列
+		for _, neighbor := range graph[curNodeid] {
+			nextNodeId := neighbor[0]
+			costToNextNode := costFromSrc + neighbor[1]
+			// 中转次数消耗 1
+			nextNodeNumFromSrc := curNodeNumFromSrc + 1
+			// 更新 dp table
+			if distTO[nextNodeId] > costToNextNode {
+				distTO[nextNodeId] = costToNextNode
+				nodeNumTO[nextNodeId] = nextNodeNumFromSrc
+			} // 剪枝，如果中转次数更多，花费还更大，那必然不会是最短路径
+			if costToNextNode > distTO[nextNodeId] &&
+				nextNodeNumFromSrc > nodeNumTO[nextNodeId] {
+				continue
+			}
+			heap.Push(pq, State{nextNodeId, costToNextNode, nextNodeNumFromSrc})
+		}
+	}
+	return -1
+}
 func Test_cheapest_flights_within_k_stops(t *testing.T) {
-	n, edges := 3, [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}
-	src, dst, k := 0, 2, 0
+	edges := [][]int{{0, 4, 5}, {0, 3, 8}, {0, 1, 9},
+		{4, 5, 6}, {3, 5, 2}, {1, 2, 2}, {2, 5, 1}}
+	n, src, dst, k := 6, 0, 5, 3
 	t.Log(findCheapestPrice_rec(n, edges, src, dst, k))
 	t.Log(findCheapestPrice_ite(n, edges, src, dst, k))
+	t.Log(findCheapestPrice_Dijkstra(n, edges, src, dst, k))
+}
+
+func Benchmark_cheapest_flights_within_k_stops(b *testing.B) {
+	edges := [][]int{{0, 4, 5}, {0, 3, 8}, {0, 1, 9},
+		{4, 5, 6}, {3, 5, 2}, {1, 2, 2}, {2, 5, 1}}
+	n, src, dst, k := 6, 0, 5, 3
+	b.Run("ite", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			findCheapestPrice_ite(n, edges, src, dst, k)
+		}
+		b.StopTimer()
+	})
+	b.Run("rec", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			findCheapestPrice_rec(n, edges, src, dst, k)
+		}
+		b.StopTimer()
+	})
+	b.Run("dijkstra", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			findCheapestPrice_Dijkstra(n, edges, src, dst, k)
+		}
+		b.StopTimer()
+	})
 }
