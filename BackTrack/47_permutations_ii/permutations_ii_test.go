522689eb5b912e3b58334b9c1bf8be63ea3796ac
winkman <cralack@hotmail.com>
2022-04-08 12:04:22 +0800

    add comment

--- BackTrack/47_permutations_ii/permutations_ii_test.go
+++ BackTrack/47_permutations_ii/permutations_ii_test.go
@@ -6,32 +6,44 @@ import (
 )
 
 func permuteUnique(nums []int) (res [][]int) {
-	var dfs func(start int)
-	dfs = func(start int) {
+	// 先排序，让相同的元素靠在一起
+	sort.Ints(nums)
+	path := make([]int, 0)
+	used := make([]bool, len(nums))
+	var dfs func()
+	dfs = func() {
 		//base case
-		if start == len(nums) {
+		if len(path) == len(nums) {
 			tmp := make([]int, len(nums))
-			copy(tmp, nums)
+			copy(tmp, path)
 			res = append(res, tmp)
 			//fmt.Println(tmp)
 			return
 		}
-		for i := start; i < len(nums); i++ {
-			if i > start && nums[i] == nums[i-1] {
+		for i := 0; i < len(nums); i++ {
+			if used[i] {
 				continue
 			}
-			nums[start], nums[i] = nums[i], nums[start]
-			dfs(start + 1)
-			nums[start], nums[i] = nums[i], nums[start]
+			// 剪枝逻辑，固定相同的元素在排列中的相对位置
+			if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
+				continue
+			}
+			used[i] = true
+			path = append(path, nums[i])
+			dfs()
+			path = path[:len(path)-1]
+			used[i] = false
 		}
 	}
-	sort.Ints(nums)
-	dfs(0)
+
+	dfs()
 	return res
 }
 func Test_permutations_ii(t *testing.T) {
 	nums := []int{1, 2, 3}
 	t.Log(permuteUnique(nums))
 	nums = []int{1, 1, 2}
+	t.Log(permuteUnique(nums))
+	nums = []int{0, 1, 0, 0, 9}
 	t.Log(permuteUnique(nums))
 }
