ef77772226db2a7fbb182d934576d106b48341c7
winkman <cralack@hotmail.com>
2022-04-11 13:49:41 +0800

    count_primes

---
+++ MathTricks/204_count_primes/count_primes_test.go
@@ -0,0 +1,23 @@
+package countprimes
+
+import "testing"
+
+func countPrimes(n int) (cnt int) {
+	notPrime := make([]bool, n)
+	for i := 2; i*i < n; i++ { //只需要遍历 [2,sqrt(n)]
+		if !notPrime[i] {
+			for j := i * i; j < n; j += i { //from i*i
+				notPrime[j] = true
+			}
+		}
+	}
+	for i := 2; i < n; i++ {
+		if !notPrime[i] {
+			cnt++
+		}
+	}
+	return cnt
+}
+func Test_count_primes(t *testing.T) {
+	t.Log(countPrimes(10))
+}
