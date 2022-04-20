01d1fffabc08881fc164129894264b98b22751b5
winkman <cralack@hotmail.com>
2022-04-20 15:24:14 +0800

    exam_room

---
+++ ClassicInterviewProblem/855_exam_room/exam_room_test.go
@@ -0,0 +1,78 @@
+package examroom
+
+import "testing"
+
+type ExamRoom struct {
+	N     int
+	Slice []int
+}
+
+func Constructor(n int) ExamRoom {
+	return ExamRoom{N: n, Slice: []int{}}
+}
+
+func (p *ExamRoom) Seat() int {
+	student := 0
+	idx := 0
+	if len(p.Slice) > 0 {
+		dist := p.Slice[0] //初始化个两座间最大距离
+		pre := -1          //初始化个前一座位号
+		for i, val := range p.Slice {
+			if pre != -1 {
+				d := (val - pre) / 2 //两座距离d
+				if d > dist {
+					dist = d
+					student = pre + d //对应座号
+					idx = i           //座号对应下标
+				}
+			}
+			pre = val
+		}
+		//n-1和slice末尾间的距离>dist
+		//则将n-1插入slice末尾
+		if p.N-1-p.Slice[len(p.Slice)-1] > dist {
+			student = p.N - 1
+			idx = len(p.Slice)
+		}
+	}
+	p.Slice = append(p.Slice, 0) //末尾前新增
+	copy(p.Slice[idx+1:], p.Slice[idx:])
+	p.Slice[idx] = student //并更改相应座号
+	return student
+}
+
+func (p *ExamRoom) Leave(x int) {
+	idx := 0
+	for i := 0; i < len(p.Slice); i++ {
+		if p.Slice[i] == x {
+			idx = i //确定x座号对应数组下标
+			break
+		}
+	} //对idx前后切片拼接 -> remove idx对应元素
+	p.Slice = append(p.Slice[:idx], p.Slice[idx+1:]...)
+}
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Seat();
+ * obj.Leave(p);
+ */
+func Test_exam_room(t *testing.T) {
+	cmd := []string{"ExamRoom", "seat", "seat", "seat",
+		"seat", "seat", "seat", "seat", "seat", "seat",
+		"seat", "leave", "seat"}
+	var room ExamRoom
+	for _, c := range cmd {
+		switch c {
+		case "ExamRoom":
+			room = Constructor(10)
+		case "seat":
+			room.Seat()
+		case "leave":
+			room.Leave(1)
+			room.Leave(3)
+			room.Leave(4)
+		}
+	}
+}
