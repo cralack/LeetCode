a5d12fc19ef78b1daa7a063fe099a97fd6cf2561
winkman <cralack@hotmail.com>
2022-10-12 12:07:59 +0800

    daily:817 num components

---
+++ List/817_linked_list_components_mid/linked_list_components_test.go
@@ -0,0 +1,43 @@
+package linkedlistcomponents
+
+import (
+	. "Learning/LeetCode/List/MyList"
+	"testing"
+)
+
+/**
+ * Definition for singly_linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func numComponents(head *ListNode, nums []int) (ans int) {
+	vis := make(map[int]struct{}, 0)
+	for _, n := range nums {
+		vis[n] = struct{}{}
+	}
+
+	flag := false
+	for cur := head; cur != nil; cur = cur.Next {
+		if _, has := vis[cur.Val]; has && !flag {
+			ans++
+			flag = true
+		} else if !has {
+			flag = false
+		}
+	}
+	return
+}
+
+func Test_linked_list_components(t *testing.T) {
+	head := Init([]int{0, 1, 2, 3})
+	nums := []int{0, 1, 3}
+	t.Log(numComponents(head, nums))
+	head = Init([]int{0, 1, 2, 3, 4})
+	nums = []int{0, 3, 1, 4}
+	t.Log(numComponents(head, nums))
+	head = Init([]int{1, 2, 0, 4, 3})
+	nums = []int{3, 4, 0, 2, 1}
+	t.Log(numComponents(head, nums))
+}
