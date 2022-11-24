6238c2dce4aae166392b8565b39fa9f54263e4a0
winkman <cralack@hotmail.com>
2022-11-24 14:53:40 +0800

    daily:795 numSubarrayBoundedMax mid

---
+++ Array/795_number_of_subarrays_with_bounded_maximum_mid/number_of_subarrays_with_bounded_maximum_test.go
@@ -0,0 +1,26 @@
+package numberofsubarray
+
+import "testing"
+
+func numSubarrayBoundedMax(nums []int, left, right int) (ans int) {
+	//双指针
+	i0, i1 := -1, -1
+	for i, x := range nums {
+		if x > right {
+			i0 = i
+		}
+		if x >= left {
+			i1 = i
+		}//贡献度
+		ans += i1 - i0
+	}
+	return
+}
+
+func Test_123(t *testing.T) {
+	nums, left, right := []int{2, 1, 4, 3}, 2, 3
+	t.Log(numSubarrayBoundedMax(nums, left, right))
+	nums, left, right = []int{2, 9, 2, 5, 6}, 2, 8
+	t.Log(numSubarrayBoundedMax(nums, left, right))
+
+}
