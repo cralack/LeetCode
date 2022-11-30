aeb9f728cbb76f865b28b3351aaa834b62ced1dd
winkman <cralack@hotmail.com>
2022-11-30 10:29:08 +0800

    daily:895 maximum_frequency_stack hard

--- ADT/895_maximum_frequency_stack_hard/maximum_frequency_stack_test.go
+++ ADT/895_maximum_frequency_stack_hard/maximum_frequency_stack_test.go
@@ -79,23 +79,23 @@ func (p *FreqStack) log() string {
 }
 
 func Test_maximum_frequency_stack(t *testing.T) {
-	// c1 := []string{"FreqStack", "push", "push",
-	// 	"push", "push", "push", "push",
-	// 	"pop", "pop", "pop", "pop"}
-	// c2 := []int{-1, 5, 7,
-	// 	5, 7, 4, 5,
-	// 	-1, -1, -1, -1}
-	c1 := []string{"FreqStack", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop"}
-	c2 := []int{-1, 30, 40, 4, 25, 20, 40, 48, 21, 12, 44, 1, 16, 20, 9, 34, 26, 12, 21, 35, 16, 3, 23, 9, 31, 10, 6, -1, 45, -1, 16, -1, 14, -1, 27, -1, 35, -1, 34, -1, 40, -1, 13, -1, 21, -1, 18, -1, 26, -1, 29, -1, 32, -1, 3, -1, 18, -1, 36, -1, 1, -1, 38, -1, 34, -1, 20, -1, 22, -1, 13, -1, 37, -1, 24, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+	c1 := []string{"FreqStack", "push", "push",
+		"push", "push", "push", "push",
+		"pop", "pop", "pop", "pop"}
+	c2 := []int{-1, 5, 7,
+		5, 7, 4, 5,
+		-1, -1, -1, -1}
+	// c1 := []string{"FreqStack", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "push", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop", "pop"}
+	// c2 := []int{-1, 30, 40, 4, 25, 20, 40, 48, 21, 12, 44, 1, 16, 20, 9, 34, 26, 12, 21, 35, 16, 3, 23, 9, 31, 10, 6, -1, 45, -1, 16, -1, 14, -1, 27, -1, 35, -1, 34, -1, 40, -1, 13, -1, 21, -1, 18, -1, 26, -1, 29, -1, 32, -1, 3, -1, 18, -1, 36, -1, 1, -1, 38, -1, 34, -1, 20, -1, 22, -1, 13, -1, 37, -1, 24, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
 	var stack FreqStack
 	for idx, str := range c1 {
-		sb := &strings.Builder{}
-		sb.WriteString(strconv.Itoa(idx))
-		sb.WriteString(": c1:")
-		sb.WriteString(str)
-		sb.WriteString(" ")
-		sb.WriteString(strconv.Itoa(c2[idx]))
-		WriteLog.WriteFile("max_freq_stack", sb.String())
+		// sb := &strings.Builder{}
+		// sb.WriteString(strconv.Itoa(idx))
+		// sb.WriteString(": c1:")
+		// sb.WriteString(str)
+		// sb.WriteString(" ")
+		// sb.WriteString(strconv.Itoa(c2[idx]))
+		// WriteLog.WriteFile("max_freq_stack", sb.String())
 		switch str {
 		case "FreqStack":
 			stack = Constructor()
