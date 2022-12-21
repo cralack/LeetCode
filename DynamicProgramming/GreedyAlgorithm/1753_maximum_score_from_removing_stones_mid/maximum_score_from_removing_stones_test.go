9e98410575c83edfae380faf96787886f828bc18
winkman <cralack@hotmail.com>
2022-12-21 16:11:16 +0800

    daily:1753 maximum_score_from_removing_stones mid

---
+++ DynamicProgramming/GreedyAlgorithm/1753_maximum_score_from_removing_stones_mid/maximum_score_from_removing_stones_test.go
@@ -0,0 +1,35 @@
+package maximumscorefromremovingstones
+
+import (
+	"sort"
+	"testing"
+)
+
+func maximumScore(a int, b int, c int) (ans int) {
+	arr := []int{a, b, c}
+	sort.Ints(arr)
+	for arr[1] > 0 {
+		ans++
+		arr[1]--
+		arr[2]--
+		sort.Ints(arr)
+	}
+	return
+}
+func maximumScore_math(a int, b int, c int) (ans int) {
+	arr := []int{a, b, c}
+	sort.Ints(arr)
+	//triangle?
+	if arr[0]+arr[1] <= arr[2] {
+		return arr[0] + arr[1]
+	}
+	return (a + b + c) >> 1
+}
+func Test_maximum_score_from_removing_stones(t *testing.T) {
+	a, b, c := 2, 4, 6
+	t.Log(maximumScore(a, b, c))
+	a, b, c = 4, 4, 6
+	t.Log(maximumScore(a, b, c))
+	a, b, c = 1, 8, 8
+	t.Log(maximumScore(a, b, c))
+}
