8c33165be1fad29d6fc01907d6f201a0e54f4b29
winkman <cralack@hotmail.com>
2023-02-05 13:07:36 +0800

    choare:rename file

--- List/141and142_Linked_list_cycle/141_Linked_list_cycle_test.go
+++ List/141and142_Linked_list_cycle/141_Linked_list_cycle_test.go
@@ -1,32 +1,9 @@
 package linkedlistcycle
 
 import (
-	"fmt"
+	. "LeetCode/List/MyList"
 	"testing"
 )
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-func (head *ListNode) Init(arr []int) {
-	cur := head
-	cur.Val = arr[0]
-	for i := 1; i < len(arr); i++ {
-		cur.Next = &ListNode{Val: arr[i]}
-		cur = cur.Next
-	}
-}
-
-func (head *ListNode) Show() {
-	cur := head
-	for cur != nil {
-		fmt.Printf("%d ", cur.Val)
-		cur = cur.Next
-	}
-	fmt.Printf("\n")
-}
 
 func hasCycle(head *ListNode) bool {
 	pre, cur := head, head
@@ -61,16 +38,12 @@ func detectCycle(head *ListNode) *ListNode {
 }
 
 func TestListCycle(t *testing.T) {
-	arr1 := []int{3, 2, 0, -4}
-	list1 := new(ListNode)
-	list1.Init(arr1)
+	list1 := Init([]int{3, 2, 0, -4})
 	list1.Next.Next.Next.Next = list1.Next
 	res1 := detectCycle(list1)
 	t.Log(res1.Val)
 
-	arr2 := []int{1, 2}
-	list2 := new(ListNode)
-	list2.Init(arr2)
+	list2 := Init([]int{1, 2})
 	list2.Next.Next = list2
 	res2 := detectCycle(list2)
 	t.Log(res2.Val)
