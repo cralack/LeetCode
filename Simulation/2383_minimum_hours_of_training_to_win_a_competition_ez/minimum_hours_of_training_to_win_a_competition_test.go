52948a84f91ce2e34b1750108c091c15512bf18f
winkman <cralack@hotmail.com>
2023-03-13 14:54:32 +0800

    daily:2383 minimum_hours_of_training_to_win ez

---
+++ Simulation/2383_minimum_hours_of_training_to_win_a_competition_ez/minimum_hours_of_training_to_win_a_competition_test.go
@@ -0,0 +1,40 @@
+package minimumhoursoftrainingtowin
+
+import "testing"
+
+func minNumberOfHours(initialEnergy int, initialExperience int, energy []int, experience []int) (ans int) {
+	s := 0
+	for _, x := range energy {
+		s += x
+	}
+	if dif := s - initialEnergy + 1; dif > 0 {
+		ans += dif
+	}
+	for _, x := range experience {
+		if initialExperience <= x {
+			ans += x - initialExperience + 1
+			initialExperience = x + 1
+		}
+		initialExperience += x
+	}
+	return
+}
+
+func Test_minimum_hours_of_training_to_win_a_competition(t *testing.T) {
+	//test table
+	tests := []struct {
+		iiEne, iiExp int
+		ene, exp     []int
+	}{
+		{5, 3, []int{1, 4, 3, 2}, []int{2, 6, 3, 1}},
+		{2, 4, []int{1}, []int{3}},
+	}
+
+	//table drive test
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			ans := minNumberOfHours(tt.iiEne, tt.iiExp, tt.ene, tt.exp)
+			t.Log(ans)
+		})
+	}
+}
