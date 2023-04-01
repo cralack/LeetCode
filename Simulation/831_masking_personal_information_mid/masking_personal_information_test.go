609857b7f52435a1f27a782ed41b176f10c1ce77
winkman <cralack@hotmail.com>
2023-04-01 14:55:50 +0800

    daily:831 masking_personal_information mid

---
+++ Simulation/831_masking_personal_information_mid/masking_personal_information_test.go
@@ -0,0 +1,45 @@
+package maskingpersonalinformation
+
+import (
+	"strings"
+	"testing"
+)
+
+func maskPII(tar string) string {
+	if idx := strings.Index(tar, "@"); idx != -1 {
+		//s is mail add
+		ans := make([]byte, 0, 2*len(tar))
+		ans = append(ans, toLower(tar[0]))
+		ans = append(ans, []byte("*****")...)
+		for i := idx - 1; i < len(tar); i++ {
+			ans = append(ans, toLower(tar[i]))
+		}
+		return string(ans)
+	} else {
+		//s is phone num
+		num := make([]byte, 0, len(tar))
+		for _, c := range tar {
+			if '0' <= c && c <= '9' {
+				num = append(num, byte(c))
+			}
+		}
+		suf := "***-***-" + string(num[len(num)-4:])
+		if len(num) == 10 {
+			return suf
+		}
+		return "+" + strings.Repeat("*", len(num)-10) + "-" + suf
+	}
+}
+func toLower(char byte) byte {
+	if 'A' <= char && char <= 'Z' {
+		char = char - 'A' + 'a'
+	}
+	return char
+}
+func Test_masking_personal_information(t *testing.T) {
+	strs := []string{"LeetCode@LeetCode.com", "AB@qq.com",
+		"1(234)567-890", "86-(10)12345678"}
+	for _, s := range strs {
+		t.Log(maskPII(s))
+	}
+}
