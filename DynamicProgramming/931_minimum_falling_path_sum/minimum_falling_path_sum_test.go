d6b9d38e6e7c3566a3419c97e9df2b66043072a3
winkman <cralack@hotmail.com>
2023-07-13 10:54:53 +0800

    daily:931 minimum_falling_path_sum mid

--- DynamicProgramming/931_minimum_falling_path_sum/minimum_falling_path_sum_test.go
+++ DynamicProgramming/931_minimum_falling_path_sum/minimum_falling_path_sum_test.go
@@ -1,48 +1,37 @@
 package minimumfallingpathsum
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func minFallingPathSum(matrix [][]int) int {
+func minFallingPathSum(matrix [][]int) (ans int) {
 	// init
 	n := len(matrix)
 	dp := make([][]int, n)
-	min := func(a, b int) int {
-		if a < b {
-			return a
-		}
-		return b
-	}
 	for i := range dp {
-		dp[i] = make([]int, n)
-		if i == 0 {
-			for j, val := range matrix[i] {
-				dp[i][j] = val
-			}
+		dp[i] = make([]int, n+2)
+		dp[i][0], dp[i][n+1] = math.MaxInt, math.MaxInt
+	}
+	copy(dp[0][1:], matrix[0])
+	for row := 1; row < n; row++ {
+		for col, x := range matrix[row] {
+			dp[row][col+1] = min(min(dp[row-1][col], dp[row-1][col+1]), dp[row-1][col+2]) + x
 		}
 	}
-	minVal := 999999
-	// dp
-	for i := 1; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if j == 0 { // left side
-				dp[i][j] = min(dp[i-1][j], dp[i-1][j+1]) + matrix[i][j]
-			} else if j == n-1 { // right side
-				dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + matrix[i][j]
-			} else {
-				dp[i][j] = min(min(dp[i-1][j], dp[i-1][j-1]),
-					dp[i-1][j+1]) + matrix[i][j]
-			}
-		}
+	ans = math.MaxInt
+	for _, x := range dp[n-1] {
+		ans = min(ans, x)
 	}
-	// check min
-	for k := 0; k < n; k++ {
-		if minVal > dp[n-1][k] {
-			minVal = dp[n-1][k]
-		}
-	}
-
-	return minVal
+	return
 }
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func Test_minimum_falling_path_sum(t *testing.T) {
 	matrix := [][]int{{2, 1, 3}, {6, 5, 4}, {7, 8, 9}}
 	t.Log(minFallingPathSum(matrix))
