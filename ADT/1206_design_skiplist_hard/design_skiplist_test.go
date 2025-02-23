5a25f800ffaf4fab29697f0a2f32a76dd1cf0600
winkman <cralack@hotmail.com>
2025-02-23 11:37:37 +0800

    daily:1206 design skiplist hard

--- ADT/1206_design_skiplist_hard/design_skiplist_test.go
+++ ADT/1206_design_skiplist_hard/design_skiplist_test.go
@@ -3,69 +3,99 @@ package designskiplisthard
 import (
 	"math/rand"
 	"testing"
+	"time"
 )
 
-type SkipNode struct {
-	Val  int
-	Next []*SkipNode
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const (
+	maxLevel = 16
+	p        = 1 / 2.718281828459045
+)
+
+type node struct {
+	val  int
+	next []*node
 }
+
+func newNode(val, level int) *node {
+	return &node{val: val, next: make([]*node, level)}
+}
+
 type Skiplist struct {
-	Level int
-	Head  *SkipNode
+	head  *node
+	level int
 }
 
 func Constructor() Skiplist {
-	return Skiplist{Level: 10,
-		Head: &SkipNode{Val: -1, Next: make([]*SkipNode, 10)}}
+	return Skiplist{head: newNode(-1, maxLevel), level: 1}
 }
 
-func (this *Skiplist) Find(target int, Prevs []*SkipNode) {
-	// 找出target在跳表中每一层的Prev节点并储存于Prevs数组中
-	cur := this.Head
-	for i := this.Level - 1; i >= 0; i-- { // 向下遍历
-		for cur.Next[i] != nil && cur.Next[i].Val < target { // 严格<target
-			cur = cur.Next[i]
+func (p *Skiplist) Search(target int) bool {
+	cur := p.head
+	for i := p.level - 1; i >= 0; i-- {
+		cur = findCloset(cur, i, target)
+		if cur.next[i] != nil && cur.next[i].val == target {
+			return true
 		}
-		Prevs[i] = cur
-	} // 查找效率 O(logN)
+	}
+	return false
 }
 
-func (this *Skiplist) Search(target int) bool {
-	Prevs := make([]*SkipNode, this.Level)
-	this.Find(target, Prevs)
-
-	// 先判最底层 <target 的节点后继不为nil
-	// 后判 val==target
-	return Prevs[0].Next[0] != nil && Prevs[0].Next[0].Val == target
-}
-
-func (this *Skiplist) Add(target int) {
-	Prevs := make([]*SkipNode, this.Level)
-	this.Find(target, Prevs)
-
-	node := &SkipNode{Val: target, Next: make([]*SkipNode, this.Level)}
-	for i := 0; i < this.Level; i++ { // 由底向上
-		node.Next[i] = Prevs[i].Next[i] // 连接target节点与跳表每层
-		Prevs[i].Next[i] = node
-		if rand.Intn(2) == 0 { // (1/2)^level概率被提取到上层
-			break
+func (p *Skiplist) Add(num int) {
+	level := randomLevel()
+	if level > p.level {
+		p.level = level
+	}
+	tmp := newNode(num, level)
+	cur := p.head
+	for i := p.level - 1; i >= 0; i-- {
+		cur = findCloset(cur, i, num)
+		if i < level {
+			tmp.next[i] = cur.next[i]
+			cur.next[i] = tmp
 		}
 	}
 }
 
-func (this *Skiplist) Erase(target int) bool {
-	Prevs := make([]*SkipNode, this.Level)
-	this.Find(target, Prevs)
-
-	node := Prevs[0].Next[0]               // 待删节点
-	if node == nil || node.Val != target { // 判空
-		return false
+func (p *Skiplist) Erase(num int) bool {
+	ok := false
+	cur := p.head
+	for i := p.level - 1; i >= 0; i-- {
+		cur = findCloset(cur, i, num)
+		if cur.next[i] != nil && cur.next[i].val == num {
+			cur.next[i] = cur.next[i].next[i]
+			ok = true
+		}
 	}
-	for i := 0; i < this.Level && Prevs[i].Next[i] == node; i++ {
-		Prevs[i].Next[i] = node.Next[i] // 由下至上逐层断链
+	for p.level > 1 && p.head.next[p.level-1] == nil {
+		p.level--
 	}
-	return true
+	return ok
 }
+
+func findCloset(cur *node, level, target int) *node {
+	for cur.next[level] != nil && cur.next[level].val < target {
+		cur = cur.next[level]
+	}
+	return cur
+}
+
+func randomLevel() int {
+	level := 1
+	for level < maxLevel && rng.Float64() < p {
+		level++
+	}
+	return level
+}
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Search(target);
+ * obj.Add(num);
+ * param_3 := obj.Erase(num);
+ */
 
 /**
  * Your Skiplist object will be instantiated and called as such:
