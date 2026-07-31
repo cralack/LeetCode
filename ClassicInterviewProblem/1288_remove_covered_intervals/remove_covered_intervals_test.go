0e7203b4ce0a727db008e30720c6028b9c85d5ee
winkman <cralack@hotmail.com>
2026-07-31 21:13:18 +0800

    daily:1288 remove_covered_intervals mid

--- ClassicInterviewProblem/1288_remove_covered_intervals/remove_covered_intervals_test.go
+++ ClassicInterviewProblem/1288_remove_covered_intervals/remove_covered_intervals_test.go
@@ -1,33 +1,38 @@
 package removecoveredintervals
 
 import (
-	"cmp"
-	"slices"
+	"sort"
 	"testing"
 )
 
-func removeCoveredIntervals(intervals [][]int) (ans int) {
-	slices.SortFunc(intervals, func(a, b []int) int {
-		return cmp.Or(a[0]-b[0], b[1]-a[1])
+func removeCoveredIntervals(intervals [][]int) int {
+	n := len(intervals)
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] != intervals[j][0] {
+			return intervals[i][0] < intervals[j][0]
+		} else {
+			return intervals[i][1] > intervals[j][1]
+		}
 	})
-	maxRight := 0
-	for _, p := range intervals {
-		if p[1] > maxRight {
-			maxRight = p[1]
-			ans++
+	left, right := intervals[0][0], intervals[0][1]
+	cnt := 0
+	for i := 1; i < len(intervals); i++ { // 情况一，找到覆盖区间
+		if left <= intervals[i][0] && right >= intervals[i][1] {
+			cnt++
+		} // 情况二，找到相交区间，合并
+		if right >= intervals[i][0] && right <= intervals[i][1] {
+			right = intervals[i][1]
+		} // 情况三，完全不相交，更新起点和终点
+		if right < intervals[i][0] {
+			left = intervals[i][0]
+			right = intervals[i][1]
 		}
 	}
-	return
+	return n - cnt
 }
-
 func Test_remove_covered_intervals(t *testing.T) {
-	tests := []struct {
-		intervals [][]int
-	}{
-		{[][]int{{1, 4}, {3, 6}, {2, 8}}},
-		{[][]int{{1, 4}, {1, 2}, {3, 4}}},
-	}
-	for _, tt := range tests {
-		t.Log(removeCoveredIntervals(tt.intervals))
-	}
+	intervals := [][]int{{1, 4}, {3, 6}, {2, 8}}
+	t.Log(removeCoveredIntervals(intervals))
+	intervals = [][]int{{1, 4}, {1, 2}, {3, 4}}
+	t.Log(removeCoveredIntervals(intervals))
 }
